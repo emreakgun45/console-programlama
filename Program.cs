6f927754ca46b05731d7bd930747bc3fa659e402
Emre Akgun <emreakgun45@gmail.com>
2021-10-29 18:39:47 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace console_programlama
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("İsminizi girin: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Soyadınızı girin: ");
+            string surName = Console.ReadLine();
+
+            Console.WriteLine("Merhaba " + name + "  " + surName);
+        }
+    }
+}
